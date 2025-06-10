afd4d46ef49411b55b89d564e891886032c54619
Robson Caetano <126205855+RobsonCaetanoFerreira@users.noreply.github.com>
2025-06-10 09:15:30 -0300

    Create ex9 idade.py

---
+++ ex9 idade.py
@@ -0,0 +1,8 @@
+print('-'*15, 'IDADE MINIMA', '-'*15)
+
+idade = int(input('Informe sua idade: '))
+
+if idade > 18:
+    print(f'Com {idade} voçê pode digirir.')
+else:
+    print(f'Com {idade} você não pode dirigir.')
